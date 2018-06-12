41710119715b6d509aa143f3d3738f5286be0868
Leonardo Rojas <lrojas@funx.io>
2018-06-11 23:52:07 -0400

    Article resource, model and create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get 'welcome/index'
   get 'welcome/index', to: 'welcome#index'
   get 'welcome/sum'
+  resources :articles
   root 'welcome#index'
   # get 'signup', to: 'accounts#new', as: 'signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
