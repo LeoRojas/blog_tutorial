656cda194f27570fed41da5c256bbfe48bf164bc
Leonardo Rojas <lrojas@funx.io>
2018-06-16 11:05:53 -0400

    previous class

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,21 @@
 class Article < ApplicationRecord
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 5 }
+
+  # def there_is_stock
+  # end
+
+  # def get_category
+  # end
+
+  # def set_category
+  # end
+
+  # def article_by_author
+  # end
+
+  # def set_age
+  #   age = function to get age from birthdate
+  #   self.age = age
+  # end
 end
