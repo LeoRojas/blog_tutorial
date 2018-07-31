282bac74e86261ed21de6afdaf0cafc78c964c3a
Leonardo Rojas <lrojas@funx.io>
2018-07-30 23:23:04 -0400

    Added bootstrap and styled some of Articles views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   # get 'welcome/index'
   get 'welcome/index', to: 'welcome#index'
   get 'welcome/sum'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614023733) do
+ActiveRecord::Schema.define(version: 20180731021229) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20180614023733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
